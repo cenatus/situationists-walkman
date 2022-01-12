51a97998d2735e556c026638b71e9e672b626822
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2022-01-12 18:01:33 +0100

    Add AppDelegate as we'll need it to initialize ARGeo etc
    following: https://www.hackingwithswift.com/quick-start/swiftui/how-to-add-an-appdelegate-to-a-swiftui-app

---
+++ SituationistsWalkman/SituationistsWalkman/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  SituationistsWalkman
+//
+//  Created by Tim on 12/1/22.
+//
+
+import Foundation
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+}
