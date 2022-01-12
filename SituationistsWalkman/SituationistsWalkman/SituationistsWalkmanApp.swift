48f55b2c4295a6d98a4339f7e81505aed14b26c8
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2022-01-12 17:27:11 +0100

    empty swiftui project

---
+++ SituationistsWalkman/SituationistsWalkman/SituationistsWalkmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SituationistsWalkmanApp.swift
+//  SituationistsWalkman
+//
+//  Created by Tim on 12/1/22.
+//
+
+import SwiftUI
+
+@main
+struct SituationistsWalkmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
