c374c71be49f5deed662bb256f549ebfaeaa44cf
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2022-01-12 17:43:50 +0100

    Add an ARView following DevGenius article
    Basically everything here as a first stub:
    
    https://blog.devgenius.io/implementing-ar-in-swiftui-without-storyboards-ec529ace7ab2

---
+++ SituationistsWalkman/SituationistsWalkman/ARView.swift
@@ -0,0 +1,73 @@
+//
+//  ARView.swift
+//  SituationistsWalkman
+//
+//  Created by Tim on 12/1/22.
+//
+
+import Foundation
+import ARKit
+import SwiftUI
+
+// MARK: - ARViewIndicator
+struct ARViewIndicator: UIViewControllerRepresentable {
+    typealias UIViewControllerType = ARView
+    
+    func makeUIViewController(context: Context) -> ARView {
+        return ARView()
+    }
+    
+    func updateUIViewController(_ uiViewController:
+                                ARViewIndicator.UIViewControllerType, context:
+                                UIViewControllerRepresentableContext<ARViewIndicator>) { }
+}
+
+// MARK: - ARView
+
+class ARView: UIViewController, ARSCNViewDelegate {
+    
+    var arView: ARSCNView {
+        return self.view as! ARSCNView
+    }
+    
+    override func loadView() {
+        self.view = ARSCNView(frame: .zero)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        arView.delegate = self
+        arView.scene = SCNScene()
+    }
+    
+    // MARK: - Functions for standard AR view handling
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARWorldTrackingConfiguration()
+        arView.session.run(configuration)
+        arView.delegate = self
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        arView.session.pause()
+    }
+    
+    // MARK: - ARSCNViewDelegate
+    
+    func sessionWasInterrupted(_ session: ARSession) {}
+    
+    func sessionInterruptionEnded(_ session: ARSession) {}
+    
+    func session(_ session: ARSession, didFailWithError error: Error)
+    {}
+    
+    func session(_ session: ARSession, cameraDidChangeTrackingState
+                 camera: ARCamera) {}
+}
--- SituationistsWalkman/SituationistsWalkman/ContentView.swift
+++ SituationistsWalkman/SituationistsWalkman/ContentView.swift
@@ -7,10 +7,46 @@
 
 import SwiftUI
 
+// MARK: - NavigationIndicator
+struct NavigationIndicator: UIViewControllerRepresentable {
+    typealias UIViewControllerType = ARView
+    
+    func makeUIViewController(context: Context) -> ARView {
+        return ARView()
+    }
+    
+    func updateUIViewController(_ uiViewController:
+                                NavigationIndicator.UIViewControllerType, context:
+                                UIViewControllerRepresentableContext<NavigationIndicator>) { }
+}
+
+// MARK - ContentView
+
 struct ContentView: View {
+    @State var page = "Home"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            if page == "Home" {
+                Button("Switch to ARView") {
+                    self.page = "ARView"
+                }
+            } else if page == "ARView" {
+                ZStack {
+                    NavigationIndicator()
+                    VStack {
+                        Spacer()
+                        Spacer()
+                        Button("Home") {
+                            self.page = "Home"
+                        }.padding()
+                            .background(RoundedRectangle(cornerRadius: 10)
+                                            .foregroundColor(Color.white).opacity(0.7))
+                    }
+                }
+            }
+            
+        }
     }
 }
 
